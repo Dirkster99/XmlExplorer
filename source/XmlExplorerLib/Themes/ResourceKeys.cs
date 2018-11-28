546acd4a9aae63fcab64d6020f8dc8f77536c83a
Dirkster99 <Dirk.Bahle@gmx.de>
2018-11-28 20:17:00 +0100

    Seperated out XPathTreeViewItem XAML definiton and assigned key-shortcuts for frequently used functions (see Menu Shortcuts for details).

---
+++ source/XmlExplorerLib/Themes/ResourceKeys.cs
@@ -0,0 +1,14 @@
+﻿namespace XmlExplorerLib.Themes
+{
+    using System.Windows;
+
+    /// <summary>
+    /// Class implements static resource keys that should be referenced to configure
+    /// colors, styles and other elements that are typically changed between themes.
+    /// </summary>
+    public static class ResourceKeys
+    {
+        public static readonly ComponentResourceKey XmlDelimiterStyleKey = new ComponentResourceKey(typeof(ResourceKeys), "XmlDelimiterStyleKey");
+        public static readonly ComponentResourceKey XmlNameStyleKey = new ComponentResourceKey(typeof(ResourceKeys), "XmlNameStyleKey");      
+    }
+}
