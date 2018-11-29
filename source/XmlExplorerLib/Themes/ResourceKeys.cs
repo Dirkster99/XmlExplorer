fc4691bf7bcabffb4d8a919f65d37305d9345bb4
Dirkster99 <Dirk.Bahle@gmx.de>
2018-11-29 18:05:17 +0100

    Introduced Style ResourceKeys to better track their usage and moved converters and template seletors into control library ...

--- source/XmlExplorerLib/Themes/ResourceKeys.cs
+++ source/XmlExplorerLib/Themes/ResourceKeys.cs
@@ -8,7 +8,36 @@
     /// </summary>
     public static class ResourceKeys
     {
+        // GlyphBrush -> GlyphBrushKey
+        public static readonly ComponentResourceKey GlyphBrushKey = new ComponentResourceKey(typeof(ResourceKeys), "GlyphBrushKey");
+
+        #region Style Keys
+        // ExpandCollapseToggleStyle -> ExpandCollapseToggleStyleKey
+        public static readonly ComponentResourceKey ExpandCollapseToggleStyleKey = new ComponentResourceKey(typeof(ResourceKeys), "ExpandCollapseToggleStyleKey");
+
+        // TreeViewItemFocusVisual -> TreeViewItemFocusVisualStyleKey
+        public static readonly ComponentResourceKey TreeViewItemFocusVisualStyleKey = new ComponentResourceKey(typeof(ResourceKeys), "TreeViewItemFocusVisualStyleKey");
+
+        // xmlDelimiterStyle -> XmlDelimiterStyleKey
         public static readonly ComponentResourceKey XmlDelimiterStyleKey = new ComponentResourceKey(typeof(ResourceKeys), "XmlDelimiterStyleKey");
-        public static readonly ComponentResourceKey XmlNameStyleKey = new ComponentResourceKey(typeof(ResourceKeys), "XmlNameStyleKey");      
+
+        // xmlAttributeValueStyle -> XmlAttributeValueStyleKey
+        public static readonly ComponentResourceKey XmlAttributeValueStyleKey = new ComponentResourceKey(typeof(ResourceKeys), "XmlAttributeValueStyleKey");
+
+        // xmlAttributeNameStyle -> XmlAttributeNameStyleKey
+        public static readonly ComponentResourceKey XmlAttributeNameStyleKey = new ComponentResourceKey(typeof(ResourceKeys), "XmlAttributeNameStyleKey");
+
+        // xmlNameStyle -> XmlNameStyleKey
+        public static readonly ComponentResourceKey XmlNameStyleKey = new ComponentResourceKey(typeof(ResourceKeys), "XmlNameStyleKey");
+
+        // xmlTextStyle -> XmlTextStyleKey
+        public static readonly ComponentResourceKey XmlTextStyleKey = new ComponentResourceKey(typeof(ResourceKeys), "XmlTextStyleKey");
+
+        // xmlCommentStyle -> XmlCommentStyleKey
+        public static readonly ComponentResourceKey XmlCommentStyleKey = new ComponentResourceKey(typeof(ResourceKeys), "XmlCommentStyleKey");
+
+        // xmlProcessingInstructionStyle -> XmlProcessingInstructionStyleKey
+        public static readonly ComponentResourceKey XmlProcessingInstructionStyleKey = new ComponentResourceKey(typeof(ResourceKeys), "XmlProcessingInstructionStyleKey");
+        #endregion Style Keys
     }
 }
