7e483ed5eefc753bff95041403f50e3575041cc2
Dirkster99 <Dirk.Bahle@gmx.de>
2018-11-30 17:43:41 +0100

    Project Cleaning

--- source/CleanAll.bat
+++ source/CleanAll.bat
@@ -7,6 +7,11 @@ ECHO corresponding BIN and OBJ Folder contained in the following projects
 ECHO.
 ECHO XmlExplorerDemo
 ECHO GenericXmlExplorerDemo
+ECHO 
+ECHO Components\Settings\Settings
+ECHO Components\Settings\SettingsModel
+ECHO Components\XmlExplorerLib
+ECHO Components\XmlExplorerVMLib
 ECHO.
 REM Ask the user if hes really sure to continue beyond this point XXXXXXXX
 set /p choice=Are you sure to continue (Y/N)?
@@ -42,6 +47,34 @@ RMDIR /S /Q Components\XmlExplorerLib\obj
 
 ECHO.
 ECHO Deleting .vs and BIN, OBJ Folders in Components\XmlExplorerVMLib project folder
+ECHO.
+
+RMDIR /S /Q Components\XmlExplorerVMLib\bin
+RMDIR /S /Q Components\XmlExplorerVMLib\obj
+
+ECHO.
+ECHO Deleting .vs and BIN, OBJ Folders in Components\Settings\Settings project folder
+ECHO.
+
+RMDIR /S /Q Components\Settings\Settings\bin
+RMDIR /S /Q Components\Settings\Settings\obj
+
+ECHO.
+ECHO Deleting .vs and BIN, OBJ Folders in Components\Settings\SettingsModel project folder
+ECHO.
+
+RMDIR /S /Q Components\Settings\SettingsModel\bin
+RMDIR /S /Q Components\Settings\SettingsModel\obj
+
+ECHO.
+ECHO Deleting .vs and BIN, OBJ Folders in  project folder
+ECHO.
+
+RMDIR /S /Q Components\XmlExplorerLib\bin
+RMDIR /S /Q Components\XmlExplorerLib\obj
+
+ECHO.
+ECHO Deleting .vs and BIN, OBJ Folders in  project folder
 ECHO.
 
 RMDIR /S /Q Components\XmlExplorerVMLib\bin
